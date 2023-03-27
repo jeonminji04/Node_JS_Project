d7f9dec74e1b240c110d793dd805ba18673eeafd
jeonminji04 <jeonminji04@gmail.com>
2023-03-27 16:02:06 +0900

    랭킹 진행중

--- ExpressProject/Assets/Scenes/Script/basicProtocols.cs
+++ ExpressProject/Assets/Scenes/Script/basicProtocols.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class bassicProtoclos : MonoBehaviour
+public class bassicProtocols : MonoBehaviour
 {
     //전송 1개의 단위를 Packets 이라고 클래스 선언
    public class Packets
